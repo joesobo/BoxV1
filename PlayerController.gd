338a16552612649d25468abe325da6daf26cc446
joesobo <soboleskijoe@gmail.com>
2023-03-02 14:25:06 -0800

    player jump

--- PlayerController.gd
+++ PlayerController.gd
@@ -1,12 +1,20 @@
-extends RigidBody2D
+extends CharacterBody2D
 
-@export var speed = 400
-var velocity = Vector2.ZERO
+@export var speed = 600
+@export var jumpForce = 2000
 
 func get_input():
-	var input_direction = Input.get_vector("left", "right", "jump", "down")
-	velocity = input_direction * speed
+	var horizontal_movement = Input.get_vector("left", "right", "", "")
+	var vertical_movement = Input.get_vector("", "", "jump", "down")
 
-func _physics_process(delta):
+	velocity.x = horizontal_movement.x * speed
+
+	if is_on_floor():
+		velocity.y = vertical_movement.y * jumpForce
+	else:
+		velocity.y += 98
+
+func _physics_process(_delta):
 	get_input()
-	move_and_collide(velocity * delta)
+	move_and_slide()
+
